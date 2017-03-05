fced7ecf907f065ea0c884e985bd3ee219377dee
zacshilling <zac@shilling.id.au>
2017-03-05 22:17:40 +1100

    project upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function navMenu() {
+    document.write(<a href="index.html"><button>Home</button></a>);
+    document.write(<a href="regions.html"><button>Regions</button></a>);
+    document.write(<button>Races</button>);
+    document.write(<button>Cities</button>);
+    document.write(<a href="timeline.html"><button>Timeline</button></a>);
+}
+
+document.write("Hello World!");
